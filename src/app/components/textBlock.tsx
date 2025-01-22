11ab59db05262d5ed038478ba5ee7684599a5478
mat <davy.mathew@gmail.com>
2025-01-22 15:48:45 +1300

    ohboy

--- src/app/components/textBlock.tsx
+++ src/app/components/textBlock.tsx
@@ -12,10 +12,12 @@ export default function TextBlock({
 }) {
   return (
     <div id={id} className="w-full mt-8 md:mt-12">
-      <div className="flex  h-[30px] w-full items-center ">
+      <div className="flex  h-[30px] w-full items-center justify-center">
         <h1 className=" whitespace-nowrap text-[1.25rem] md:text-[2rem]">{title}</h1>
       </div>
-      <div className="text-[0.75rem] md:text-[1rem] mt-2">{body}</div>
+      <div className="text-[0.75rem] md:text-[1rem] mt-2 text-center ml-auto mr-auto w-fit">
+        {body}
+      </div>
     </div>
   );
 }
